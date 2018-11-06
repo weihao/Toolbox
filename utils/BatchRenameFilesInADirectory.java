4677b67f0323b3792e6e4e0f06e573152a9dc1eb
25 <31719556+25@users.noreply.github.com>
2018-11-06 11:33:33 -0500

    Update BatchRenameFilesInADirectory.java

--- utils/BatchRenameFilesInADirectory.java
+++ utils/BatchRenameFilesInADirectory.java
@@ -1,16 +1,23 @@
 import java.io.File;
+import java.util.Arrays;
 
 public class BatchRenameFilesInADirectory {
     public static void main(String[] args) {
-        // Linux Path.
-        String path = "/home/Test/";
+        String path = "/home/temp/";
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
+        // Sort all files by name to keep consistency.
+        Arrays.sort(listOfFiles);
+
 
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
                 File f = new File(path + listOfFiles[i].getName());
-                String filename = listOfFiles[i].getName().replaceAll("", "");
+
+                // Rename files logic here.
+                String filename = listOfFiles[i].getName().replaceAll("req", "resp");
+
+                
                 f.renameTo(new File(path + filename));
             }
         }
